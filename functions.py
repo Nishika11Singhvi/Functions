171cf6b933d29ad8591a9d45426f14879381bd21
Nishika11Singhvi <73836493+Nishika11Singhvi@users.noreply.github.com>
2022-02-07 14:24:23 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,16 @@
+def SwapData();
+
+file1 = input("Enter the original file: ")
+file2 = input("Enter the file being swapped: ")
+
+with open(file1, 'r') as a:
+    data_a = a.read()
+with open(file2, 'r') as a:
+    data_b = b.read()
+
+with open(file1, 'w') as a:
+    a.write(data_b)
+with open(file2, 'w') as b:
+    b.write(data_a)
+
+SwapData()
